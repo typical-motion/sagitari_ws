db7c9304b8e5a3354fdf002dd4f4b844205f587c
Lss233 <i@lss233.com>
2021-05-15 08:04:39 -0700

    refactor: get raw image from camera directly

--- src/camera_recorder/src/camera_recorder_node.cpp
+++ src/camera_recorder/src/camera_recorder_node.cpp
@@ -3,14 +3,10 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <message_filters/subscriber.h>
-#include <uart_process_2/uart_receive.h>
-#include <sagitari_debug/sagitari_img_debug.h>
 #include <iostream>
 using namespace sensor_msgs;
 using namespace message_filters;
 using namespace std;
-
-ros::Publisher debugPublisher;
 
 cv::String videoNUM(cv::String Folder)
 {
@@ -34,8 +30,7 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "camera_recorder");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber originalImageSubscriber = it.subscribe("Sagitari/originalImage", 1, originalImageCallback);
-    debugPublisher = nh.advertise<uart_process_2::uart_receive>("uart_receive", 1);
+    image_transport::Subscriber originalImageSubscriber = it.subscribe("DahuaCamera/LowDims", 1, originalImageCallback);
     ros::Rate rate(150);
     while (ros::ok())
     {
