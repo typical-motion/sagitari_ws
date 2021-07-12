5e2d896e9eb19525d4a46aafe462e35347e1a009
Lss233 <i@lss233.com>
2021-07-11 18:29:04 -0700

    camera_file: add per frame play

--- src/camera_file/src/camera_file_node.cpp
+++ src/camera_file/src/camera_file_node.cpp
@@ -11,21 +11,26 @@ int main(int argc, char* argv[]) {
     cv::VideoCapture capture;
     // capture = cv::VideoCapture("/mnt/data/52.mp4");
 	// capture = cv::VideoCapture("/home/lss233/sagitari_ws/68 00_00_00-00_01_00.avi");
-	capture = cv::VideoCapture("/home/lss233/sagitari_ws/106.avi");
+	capture = cv::VideoCapture("/mnt/data/sjut 00_02_44-00_03_22.mp4");
+	// capture = cv::VideoCapture("/home/lss233/sagitari_ws/106.avi");
 	// capture = cv::VideoCapture("/mnt/data/record_27.avi");
     ros::Rate rate(30);
     cv::Mat img;
+    int freezeOnNextFrame = false;
     while(ros::ok() && capture.isOpened()) {
         capture >> img;
         cv::imshow("RAW", img);
         std_msgs::Header header;
 	    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(header, "bgr8", img).toImageMsg();
         publisher.publish(msg);
-        if(int key = cv::waitKey(25)) {
+        if(int key = cv::waitKey(freezeOnNextFrame ? 0 : 25)) {
+            freezeOnNextFrame = false;
             if(key == 'p') {
                 cv::waitKey(0);
             } else if(key == 'q') {
                 exit(0);
+            } else if(key == 'n') {
+                freezeOnNextFrame = true;
             }
         }
         ros::spinOnce();
